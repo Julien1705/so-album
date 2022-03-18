c15a79cae420e16efdd59b911b3e62bd45d9410e
unknown <julienduval17@gmail.com>
2022-03-18 12:00:53 +0100

    page principale

---
+++ view/page_principale.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Page Principale</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://bootswatch.com/5/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/page_principale.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-success m-2" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-success m-2" href="#">Log Out</a>
+                    </li>
+                </ul>
+
+                <ul class="nav navbar-nav flex-fill justify-content-center">
+                    <li class="nav-item"><a class="nav-link" href="#">Website Logo</a></li>
+                </ul>
+
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle btn btn-success m-2" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">EVENEMENTS</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">MR</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">PR</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">CT</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle btn btn-success m-2" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">ALBUM</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Transfert</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Organiser</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Visualiser</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Acces Folder</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" class="btn btn-success m-2 rounded-circle">Générer</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid content">
+        <div class="row">
+            <div class="col-4">
+                <div class="form-group mt-4">
+                    <select class="form-select" id="exampleSelect1">
+                        <option>Type de projet</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-6 mb-2">
+                        <select class="form-select" id="exampleSelect1">
+                            <option>Theme</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div class="col-6 mb-2">
+                        <select class="form-select" id="exampleSelect1">
+                            <option>Nombre de photos</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col-6 page m-1"></div>
+                            <div class="col-6 page m-1"></div>
+                        </div>
+                    </div>
+                    <div class="col-6 d-flex justify-content-center">
+                        <div class="one-page m-1"></div>
+                    </div>
+                </div>
+                <div class="dropdown-divider divider-main"></div>
+            </div>
+            <div class="col-8">
+
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
