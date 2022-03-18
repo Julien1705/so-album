2492c3c6f2ac3f95c78600ea3fafe26b41260d01
unknown <julienduval17@gmail.com>
2022-03-18 09:56:06 +0100

    initialisation

---
+++ view/choix_projet.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/choix_projet.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="choix-page">
+    <div class="form">
+        <a href="../index.php">
+            <span class="iconify" data-width="25" data-height="28" data-icon="ant-design:logout-outlined" data-rotate="270deg">
+            </span>
+        </a>
+        <form class="choix-form">
+            <label>Choisir votre projet</label>
+            <select name="" id="">
+                <option value="" selected>Votre projet
+                </option>
+                <option value="">Projet 1</option>
+                <option value="">Projet 2</option>
+                <option value="">Projet 3</option>
+            </select>
+        </form>
+        <form class="choix-form">
+            <label>Choisir votre évènement</label>
+            <select name="" id="">
+                <option value="" selected>Votre évènement
+                </option>
+                <option value="">Evènement 1</option>
+                <option value="">Evènement 2</option>
+                <option value="">Evènement 3</option>
+            </select>
+        </form>
+        <button>Continuer</button>
+    </div>
+</div>
+</body>
+
+
+</html>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+    integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+    crossorigin="anonymous"></script>
+
+<script src="https://code.iconify.design/2/2.1.2/iconify.min.js"></script>
